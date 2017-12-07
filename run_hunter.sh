cf93bb054cea5301c3255d86e9af764c6932a07c
Elliott Hall <elliott.hall@kcl.ac.uk>
2017-12-07 14:58:00 +0000

    swtiched git pull first in startup sh

--- run_hunter.sh
+++ run_hunter.sh
@@ -4,12 +4,15 @@
 # Elliott Hall
 GHOSTHUNT_DIR="/home/pi/projects/ghosthunt"
 
-# Need to be root to have access to bluetooth
-sudo su root
-source $GHOSTHUNT_DIR/ghosthunter/envs/ghosthunter/bin/activate
+# Update sources
 cd $GHOSTHUNT_DIR
 # git pull
 
 # pip update
+
+# Need to be root to have access to bluetooth
+sudo su root
+source $GHOSTHUNT_DIR/ghosthunter/envs/ghosthunter/bin/activate
+
 
 # run main script
