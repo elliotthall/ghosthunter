a2bd1b1fb02cb0a4c329e7d000f58f52c1aeb8fc
Elliott Hall <elliott.hall@kcl.ac.uk>
2018-03-23 17:56:57 +0000

    DWM get_pos working

--- demos/dwm1001.py
+++ demos/dwm1001.py
@@ -1,6 +1,8 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/python3
 import time
+
 import spidev
+
 """
 From firmware
 Pin 19
@@ -19,13 +21,14 @@ Pin 24
 CSN
 CSN
 
-Note: the connector J10 on the DWM1001 DEV board is compatible with Raspberry Pi 3 connector J8 header pins 1-26. Pin 4 from J10 provides 5V power from Raspberry Pi to the DWM1001 DEV board.
+Note: the connector J10 on the DWM1001 DEV board is compatible with Raspberry Pi 3 connector J8 header pins 1-26. Pin 
+4 from J10 provides 5V power from Raspberry Pi to the DWM1001 DEV board.
 
 """
 
-
 # "In the DWM1001 SPI scheme, the dummy bytes are octets of value 0xFF."
-DUMMY = [0xFF]
+DUMMY_BYTE = 0xFF
+DUMMY_MESSAGE = [DUMMY_BYTE]
 
 # Delay
 INIT_DELAY = 0.000005
@@ -35,7 +38,6 @@ PMSC_CONFIG_DELAY = 0.00015
 IDLE_MODE = 0x00
 RX_MODE = 0x01
 TX_MODE = 0x02
-
 # Init spi
 bus = 0
 device = 0
@@ -44,22 +46,31 @@ spi.open(0, 0)
 spi.max_speed_hz = 80000
 
 # Test message, dwm_pos_get
-msg= [0x02,0x00]
-spi.writebytes(msg)
+msg = [0x02, 0x00]
+result = spi.xfer2(msg)
+print(result)
+
+# NOTE: use tag_cfg to make sure hardware/pins ok!
 # FULL DUPLEX MUST WRITE TO READ
 # Write dummy to force read of single byte
 # If byte is nonzero, response is ready
 # read x bytes as the response
-
-	try:
-		while True:
-			# Write dummy to force read of single byte
-			resp = spi.xfer(DUMMY)
-			if resp[0] != 0:
-				# If byte is nonzero, response is ready
-				size = resp[0]
-				dummy_get =[DUMMY for x in range(1, size)]				
-				msg_return = spi.xfer(dummy_get)
-				print (msg_return)
-	except KeyboardInterrupt:
-		pass
+while True:
+    try:
+        # Write dummy to force read of single byte
+        resp = spi.xfer2(DUMMY_MESSAGE)
+        if resp[0] != 0:
+            # If byte is nonzero, response is ready
+            size = resp[0]
+            dummy_get = []
+            for x in range(1, size):
+                dummy_get.append(DUMMY_BYTE)
+            print
+            "SIZE: {}\n".format(size)
+            msg_return = spi.xfer2(dummy_get)
+            print(msg_return)
+            break
+        time.sleep(0.3)
+    except KeyboardInterrupt:
+        pass
+spi.close()
