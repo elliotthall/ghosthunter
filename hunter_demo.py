5c5dadd68ac6f0e8f8c5281c15fbef550c0cfacf
Elliott Hall <elliott.hall@kcl.ac.uk>
2018-06-01 12:30:07 +0100

    added brightness event to lightbulb poltergeist

--- hunter_demo.py
+++ hunter_demo.py
@@ -1,12 +1,12 @@
 import asyncio
 import concurrent.futures
+import json
 import logging
 import pdb
+
 import aiohttp
-import requests
-import json
-from shapely.geometry import Point
 from pyHS100 import SmartBulb
+from shapely.geometry import Point
 
 from hunter.devices import ProximityDevice
 from local import (POLTERGEIST_LOGIN, POLTERGEIST_PASSWORD)
@@ -14,6 +14,25 @@ from local import (POLTERGEIST_LOGIN, POLTERGEIST_PASSWORD)
 logging.basicConfig(
     level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+POLTERGEIST_URL = 'https://ghost-hunt.mozilla-iot.org'
+PLUG_1_URI = POLTERGEIST_URL + '/things/zwave-c83406e1-4'
+BULB_IP = '10.0.1.7'
+
+"""
+{'name': 'room1-plug1', 'type': 'smartPlug', 'description': '', 'href': 
+'/things/zwave-c83406e1-4', 'properties': {'on': {'type': 'boolean', 'href': 
+'/things/zwave-c83406e1-4/properties/on'}, 
+'instantaneousPower': {'type': 
+'number', 'unit': 'watt', 'href': 
+'/things/zwave-c83406e1-4/properties/instantaneousPower'}, 
+'voltage': {
+'type': 'number', 'unit': 'volt', 'href': 
+'/things/zwave-c83406e1-4/properties/voltage'}, 'current': {'type': 
+'number', 'unit': 'ampere', 'href': 
+'/things/zwave-c83406e1-4/properties/current'}}, 'actions': {}, 'events': {
+},
+"""
 
 
 # This will go into poltergeist when ready
@@ -41,7 +60,7 @@ class PoltergeistEvent(object):
 
 class SimpleAPIPoltergeistEvent(PoltergeistEvent):
     """If hunter intersects trigger area, send an API message """
-    poltergeist_url = 'https://ghost-hunt.mozilla-iot.org'
+    poltergeist_url = POLTERGEIST_URL
 
     api_header = {
         'Accept': 'application/json',
@@ -59,38 +78,39 @@ class SimpleAPIPoltergeistEvent(PoltergeistEvent):
     ]
 
     def __init__(self, session=None, *args, **kwargs):
-        super(SimpleAPIPoltergeistEvent,self).__init__(*args, **kwargs)
+        super(SimpleAPIPoltergeistEvent, self).__init__(*args, **kwargs)
         if session is not None:
             self.session = session
 
         # Log in and get token
+
     async def poltergeist_login(self):
-            url = self.poltergeist_url + '/login'
+        url = self.poltergeist_url + '/login'
 
-            login = {
-                'email': POLTERGEIST_LOGIN,
-                'password': POLTERGEIST_PASSWORD
-            }
-            self.session = aiohttp.ClientSession()
-            response = await self.session.post(
-                url, data=json.dumps(login), headers=self.api_header
+        login = {
+            'email': POLTERGEIST_LOGIN,
+            'password': POLTERGEIST_PASSWORD
+        }
+        self.session = aiohttp.ClientSession()
+        response = await self.session.post(
+            url, data=json.dumps(login), headers=self.api_header
+        )
+
+        if response.status == 200:
+            r = await response.json()
+            if 'jwt' in r:
+                self.bearer_token = r['jwt']
+                # Add the token to default header
+                self.api_header['Authorization'] = 'Bearer {}'.format(
+                    self.bearer_token
+                )
+            return True
+        else:
+            logging.warning('Bad connection to poltergeist code {}'.format(
+                response.status_code
             )
-
-            if response.status == 200:
-                r = await response.json()
-                if 'jwt' in r:
-                    self.bearer_token = r['jwt']
-                    # Add the token to default header
-                    self.api_header['Authorization'] = 'Bearer {}'.format(
-                        self.bearer_token
-                    )
-                return True
-            else:
-                logging.warning('Bad connection to poltergeist code {}'.format(
-                    response.status_code
-                )
-                )
-            return False
+            )
+        return False
 
     async def check_trigger(self, *args, **kwargs):
         if 'hunter_position' in kwargs:
@@ -99,21 +119,20 @@ class SimpleAPIPoltergeistEvent(PoltergeistEvent):
             for event in self.trigger_points:
                 if hunter_position.intersects(event['geometry']):
                     # We're in an event position, send api call.
-                    await self.trigger(                        
-                            hunter_position=hunter_position,
-                            event=event                        
+                    await self.trigger(
+                        hunter_position=hunter_position,
+                        event=event
                     )
                     return True
         return False
-
 
     async def trigger(self, *args, **kwargs):
         # hunter_position = kwargs['hunter_position']
         event = kwargs['event']
         await self.poltergeist_call(event['call_type'],
-                              event['uri'],
-                              data=event['json']
-                              )
+                                    event['uri'],
+                                    data=event['json']
+                                    )
         self.triggered = True
         return True
 
@@ -136,49 +155,65 @@ class SimpleAPIPoltergeistEvent(PoltergeistEvent):
                                                       headers=headers,
                                                       data=json.dumps(data)
                                                       )
-            r = await response.text()
-            print(r)
-            pdb.set_trace()
+
             return response
         except asyncio.CancelledError as cancelled:
             logging.debug('api call {} cancelled'.format(cancelled))
         return False
 
+
 # r, g, b = (111, 121, 131)
 # packed = int('%02x%02x%02x' % (r, g, b), 16)
 
 class LightBulbPoltergeistEvent(SimpleAPIPoltergeistEvent):
+    light = None
+    light_on = False
+    light_location = None
+    effect_range = 5000
+
+    def __init__(self, *args, **kwargs):
+        super(LightBulbPoltergeistEvent, self).__init__()
+        if 'light_location' in kwargs:
+            self.light_location = kwargs['light_location']
+        self.light = SmartBulb(BULB_IP)
 
     async def check_trigger(self, *args, **kwargs):
         if 'hunter_position' in kwargs:
             hunter_position = kwargs['hunter_position']
-            # Is the hunter's position intersecting with any trigger points?
-            for event in self.trigger_points:
-                if hunter_position.intersects(event['geometry']):
-                    # We're in an event position, send api call.
-                    await self.trigger(                        
-                            hunter_position=hunter_position,
-                            event=event                        
+            # get the plug 1 (radio) status and wattage
+            # if on and 0 (radio has been turned off)
+            if self.light_on is not True:
+                response = await self.poltergeist_call(
+                    'get',
+                    PLUG_1_URI + '/properties/on'
+                )
+                r = await response.json()
+                print(r)
+                pdb.set_trace()
+                if r['on'] == True:
+                    response = await self.poltergeist_call(
+                        'get',
+                        PLUG_1_URI + '/properties/instantaneousPower'
                     )
-                    return True
-        return False
-
+                    r = await response.json()
+                    wattage = r['instantaneousPower']
+                    if wattage == 0:
+                        # Turn on light
+                        self.trigger()
+            elif self.light_on is True:
+                # Light is on, change brightness for position
+                distance = hunter_position.distance(self.light_location)
+                brightness = (1 - (distance / self.effect_range)) * 100
+                self.light.brightness = brightness
+        await asyncio.sleep(0.2)
+        return True
 
     async def trigger(self, *args, **kwargs):
         # hunter_position = kwargs['hunter_position']
-        # light = SmartBulb('10.0.1.7')
         # hsv = colorsys.rgb_to_hsv(0.0,0.0,0.8)
         # conver to int!
         # light.hsv=(hsv[0]*360,hsv[1]*100,hsv[2]*100)
-        
-        # 
-
-        event = kwargs['event']
-        await self.poltergeist_call(event['call_type'],
-                              event['uri'],
-                              data=event['json']
-                              )
-        self.triggered = True
+        self.light.brightness = 50
         return True
 
 
@@ -206,13 +241,13 @@ class SymposiumHunter(ProximityDevice):
         {'id': 0,
          'geometry': Point(0, 0).buffer(500),
          'call_type': 'put',
-         'uri': poltergeist_url + '/things/zwave-c83406e1-4/properties/on',         
-         'json': {'on': True},         
+         'uri': poltergeist_url + '/things/zwave-c83406e1-4/properties/on',
+         'json': {'on': True},
          }
     ]
 
     def __init__(self, *args, **kwargs):
-        super(SymposiumHunter,self).__init__(*args, **kwargs)
+        super(SymposiumHunter, self).__init__(*args, **kwargs)
         self.poltergeist_events = [
             SimpleAPIPoltergeistEvent()
         ]
@@ -229,7 +264,7 @@ class SymposiumHunter(ProximityDevice):
         response = await self.session.post(
             url, data=json.dumps(login), headers=self.api_header
         )
-        
+
         if response.status == 200:
             r = await response.json()
             if 'jwt' in r:
@@ -261,21 +296,20 @@ class SymposiumHunter(ProximityDevice):
         if not headers:
             headers = self.api_header
         try:
-                
-                if data is None:
-                    response = await self.session.request(call_type, uri,
-                                                          headers=headers)
-                else:
-                    response = await self.session.request(call_type, uri,
-                                                          headers=headers,
-                                                          data=json.dumps(data)
-                                                          )
-                r = await response.text()
-                print(r)
-                pdb.set_trace()
-                return response
+
+            if data is None:
+                response = await self.session.request(call_type, uri,
+                                                      headers=headers)
+            else:
+                response = await self.session.request(call_type, uri,
+                                                      headers=headers,
+                                                      data=json.dumps(data)
+                                                      )
+            r = await response.text()
+            print(r)
+            return response
         except asyncio.CancelledError as cancelled:
-                logging.debug('api call {} cancelled'.format(cancelled))
+            logging.debug('api call {} cancelled'.format(cancelled))
         return False
 
     async def uwb_pos_updated(self, new_position):
@@ -292,18 +326,14 @@ class SymposiumHunter(ProximityDevice):
         #                                     thing['uri'],
         #                                     data=thing['json']
         #                                     )        
-        pdb.set_trace()
+
         for event in self.poltergeist_events:
             await event.check_trigger(
                 hunter_position=hunter_position
             )
         return True
 
-    # h = header
-    # h
-    # r = requests.get(url, headers=h)
 
-    # put to poltergeist thing
 
 
 def main():
@@ -329,7 +359,7 @@ def main():
                  'level': 0},
                  """
         websocket = loop.run_until_complete(hunter.get_ghost_server_socket())
-        hunter.websocket = websocket        
+        hunter.websocket = websocket
         try:
             hunter.bootup()
         except KeyboardInterrupt:
