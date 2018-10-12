e238f83f666c7672a644241e1ec4fd7dd10c06b5
Elliott Hall <elliott.hall@kcl.ac.uk>
2018-10-12 13:36:46 +0100

    hunter init serial

--- hunter.py
+++ hunter.py
@@ -8,15 +8,82 @@ import asyncio
 import concurrent.futures
 from concurrent.futures import CancelledError
 import logging
+import hunter.utils as utils
 from shapely.geometry import Point
+from hunter.devices import MainDevice
 logging.basicConfig(
 	level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+import pdb
+
+class GhostHunter(object):
+    """ This class contains all the various hunter detector
+         functions, merged together for ease of deployment """
+
+    microbit_serial_address = '/dev/ttyACM1'
+    microbit_serial = None
+    uwb_serial_address = '/dev/ttyACM0'
+    uwb_serial = None
+    # last position object received from DWM board
+    uwb_pos = None
+    # tolerance (in mm) to ignore so that we don't mistake
+    # fluctuation in uwb readings for hunter movement
+    uwb_tolerance = 100
+
+    def init_serial_connections(self):
+        """Establish UART connections to UWB and Micro:bit
+        Since addresses are assigned in the order deivces are connected
+        Test to make sure """
+        # Establish connections
+        first_conn = utils.connect_serial(self.uwb_serial_address)
+        second_conn = utils.connect_serial(self.microbit_serial_address)
+        # Send an id message, verify this is a DWM
+        first_conn.write(uwb.DWM_CFG_GET_MSG)
+        return_type = first_conn.read()
+
+        # Flush for safety
+        first_conn.reset_input_buffer()
+        first_conn.reset_output_buffer()
+        second_conn.reset_output_buffer()
+        second_conn.reset_input_buffer()
+        if (int.from_bytes(return_type, byteorder='little')
+                == uwb.DWM_RETURN_BYTE):
+            # Yes, assign to uwb
+            logging.debug('ACM0 assigned to uwb')
+            self.uwb_serial = first_conn
+            self.microbit_serial = second_conn
+            # todo parse the cfg get and configure here?
+        else:
+            # No, asssign to micro:bit
+            logging.debug('ACM1 assigned to uwb')
+            self.microbit_serial = first_conn
+            self.uwb_serial = second_conn
+        return True
+
+#######     Startup     #########
+
+hunter = GhostHunter()
+
+# Test and open serials
+hunter.init_serial_connections()
+
+
+
+# Main command loop
+    # Run endless loop
+    # get position
+    # message from server
+    # message from microbit
+       # Do hunt
+#finish
+# close logs
+# close serial
+
 
 
 #uwb_serial_address = '/dev/ttyACM0'
-uwb_serial_address = '/dev/tty.usbmodem1451'
-uwb_serial = serial.Serial(uwb_serial_address, 115200, timeout=3)
+#uwb_serial_address = '/dev/tty.usbmodem1451'
+#uwb_serial = serial.Serial(uwb_serial_address, 115200, timeout=3)
 #uwb.dwm_reset(uwb_serial)
 #time.sleep(5)
 #print(uwb.dwm_serial_get_loc(uwb_serial))
--- hunter/devices.py
+++ hunter/devices.py
@@ -31,9 +31,38 @@ code_lookup = {v: k for k, v in
                }
 
 
-class MainDevice(hunter_core.HunterUwbMicrobit):
+class MainDevice(object):
     """ This class contains all the various hunter detector
      functions, merged together for ease of deployment """
+
+    microbit_serial_address = '/dev/ttyACM1'
+    microbit_serial = None
+    uwb_serial_address = '/dev/ttyACM0'
+    uwb_serial = None
+    # last position object received from DWM board
+    uwb_pos = None
+    # tolerance (in mm) to ignore so that we don't mistake
+    # fluctuation in uwb readings for hunter movement
+    uwb_tolerance = 100
+    # These are shapely geometries of things the device can detect
+    # dict of lists split by level/room, updated by server as hunt develops
+    detectable_things = None
+    # Current level - in scratch this will be the room we're in
+    current_level = 0
+
+    MICROBIT_CODES = {
+        'ready': b'\x01',
+        'id': b'\x08',
+        'id_return': b'\x09',
+        'hunt': b'\x10',
+        'acc': b'\x11',
+        'toggle_acc': b'\x15',
+        'pixel': b'\x12',
+        'image': b'\x13',
+        'reset': b'\x14',
+        'data': b'\x18',
+
+    }
 
      # devices specifc codes for doing hunt work
     microbit_device_codes = {
@@ -79,13 +108,79 @@ class MainDevice(hunter_core.HunterUwbMicrobit):
                          "00000:07770:07070:07770:00000," +
                          "99999:90009:90009:90009:99999")
 
-    def extra_device_functions(self):
-        """ Add microbit, uwb listeners to loop"""
-        # todo overriden to temporarily remove ble
-        return [
-            self.microbit_listen(),
-            self.uwb_get_pos()
-        ]
+
+
+    def init_serial_connections(self):
+        """Establish UART connections to UWB and Micro:bit
+        Since addresses are assigned in the order deivces are connected
+        Test to make sure """
+        # Establish connections
+        first_conn = utils.connect_serial(self.uwb_serial_address)
+        second_conn = utils.connect_serial(self.microbit_serial_address)
+        # Send an id message, verify this is a DWM
+        first_conn.write(uwb.DWM_CFG_GET_MSG)
+        return_type = first_conn.read()
+
+        # Flush for safety
+        first_conn.reset_input_buffer()
+        first_conn.reset_output_buffer()
+        second_conn.reset_output_buffer()
+        second_conn.reset_input_buffer()
+        if (int.from_bytes(return_type, byteorder='little')
+                == uwb.DWM_RETURN_BYTE):
+            # Yes, assign to uwb
+            logging.debug('ACM0 assigned to uwb')
+            self.uwb_serial = first_conn
+            self.microbit_serial = second_conn
+            # todo parse the cfg get and configure here?
+        else:
+            # No, asssign to micro:bit
+            logging.debug('ACM1 assigned to uwb')
+            self.microbit_serial = first_conn
+            self.uwb_serial = second_conn
+        return True
+
+    def device_startup_tasks(self):
+        """1. Connect UART to micro:bit and DWM1001-DEV
+           2. Reset the micro:bit and DWM1001-DEV boards
+           3. Confirm both boards are ready
+           :return True if tasks successful
+           """
+        result = self.init_serial_connections()
+        if not result:
+            logging.error('UART connection failed!')
+            return False
+        else:
+            self.uwb_reset()
+            self.microbit_reset()
+            # Give boards time to reset
+            time.sleep(3)
+            # Query boards
+            try:
+                micro_result = self.microbit_serial.readline()
+                if self.MICROBIT_CODES['ready'] in micro_result:
+                    # Microbit ready
+                    logging.info('Micro:bit ready.')
+                else:
+                    logging.error('Micro:bit startup failed!')
+                    return False
+                uwb_cfg = uwb.dwm_serial_get_cfg(self.uwb_serial)
+                try:
+                    # UWB confirm we got a config back and it's correct
+                    # todo add further config tests
+                    if 'tag' in uwb_cfg['mode']:
+                        logging.info('DWM1001-DEV ready.')
+                except IndexError:
+                    logging.error('Getting uwb config on startup failed!')
+                    return False
+                # All done, return we are ready
+                return True
+            except asyncio.TimeoutError:
+                logging.error('Peripheral startup timed out!')
+                return False
+
+
+
 
     def detect_things(self, x, y, device_range, level=0):
         """
@@ -121,36 +216,7 @@ class MainDevice(hunter_core.HunterUwbMicrobit):
         """
 
         # create microbit detection animation based on distance
-        """
-        leds = int(
-            math.ceil(
-                (1 - (detected_thing['distance'] / settings['device_range'])) * 25
-            )
-        )
-        if leds == 0:
-            # minimum reading of one
-            leds = 1
-        # send to microbit for display
-        # todo Send result not image
-        canvas = [['0'] * 5 for x in range(0, 5)]
-        for x in range(0, leds):
-            row = int(math.floor(x / 5))
-            canvas[row][x - row * 5] = '9'
-        # no delay
-        image = "0;;"
-        for y in range(0, 5):
-            image += "".join(canvas[y])
-            if y != 4:
-                image += ":"
-        logging.info(
-            "Thing found, nearest: id {} at {}cm away, {} leds".format(
-                detected_thing['id'],
-                detected_thing['distance'],
-                leds
-            )
-        )   
-        return [self.MICROBIT_CODES['image'], image]
-        """     
+
         return [self.MICROBIT_CODES['data'],
                 str(1 - (detected_thing['distance'] / settings['device_range']))
                 ]
@@ -178,8 +244,6 @@ class MainDevice(hunter_core.HunterUwbMicrobit):
             result = self.telegraph_transmit(value)
         elif code == self.microbit_device_codes['spiritsign']:
             result = self.decode_spiritsign(value)
-        elif code == self.microbit_device_codes['radio']:
-            result = self.tune_radio(value)
         if result is not None:            
             self.microbit_write(result[0], result[1])
         del self.command_queue[self.COMMAND_HUNT]
