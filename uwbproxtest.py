b2e6dce1f218e775356843306b5ab24a25ab1ac3
Elliott Hall <elliott.hall@kcl.ac.uk>
2018-11-12 19:20:42 +0000

    new pm ecto trail

--- uwbproxtest.py
+++ uwbproxtest.py
@@ -2,8 +2,8 @@ import hunter.peripherals.uwb.uart as uwb
 import time
 import serial
 
-uwb_serial_address = '/dev/ttyACM0'
-#uwb_serial_address = '/dev/tty.usbmodem1451'
+#uwb_serial_address = '/dev/ttyACM0'
+uwb_serial_address = '/dev/tty.usbmodem1431'
 uwb_serial = serial.Serial(uwb_serial_address, 115200, timeout=3)
 #uwb.dwm_reset(uwb_serial)
 x = 0
